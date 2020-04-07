3facbeb23cc606fbaed5f38b2b2e0278a838acc3
sasie59 <sasie59@gmail.com>
2020-04-08 06:58:48 +0800

    change to  c-r-a

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dolor eligendi porro non facilis. Ipsam deserunt eaque saepe corrupti, autem voluptas dolor accusantium expedita vel facere ullam consectetur debitis fugit?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
